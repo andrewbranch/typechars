058ef4e2c4d9d022d92191875144c8996cc16204
Andrew Branch <andrew@wheream.io>
2017-11-17 14:01:09 -0800

    Initial Commit

---
+++ typechars/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  typechars
+//
+//  Created by Andrew Branch on 11/17/17.
+//  Copyright © 2017 Wheream.io. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
